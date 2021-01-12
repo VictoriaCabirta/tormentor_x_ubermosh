98eec60c60b7b818756826c8b0e7c10e78c3fe52
antonio.rodriguez <arodriguezr@fpcoruna.afundacion.org>
2021-01-12 12:12:54 +0100

    #2 Tutorial 3 acabado

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,9 +9,6 @@ def index(request):
     template = loader.get_template('polls/index.html')
     context = {'latest_question_list': latest_question_list,}
     return render(request, 'polls/index.html', context)
-    
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
