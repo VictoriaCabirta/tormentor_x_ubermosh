308077c3a160c14c0f9fdf8035edbc81196350e4
antonio.rodriguez <45874039X>
2021-01-28 20:26:21 +0100

    Registro y login casi acabados

--- Django_v1/TormentorXUbermosh_mysite/fachada_rest/rest_facade.py
+++ Django_v1/TormentorXUbermosh_mysite/fachada_rest/rest_facade.py
@@ -2,7 +2,7 @@ import json
 
 from .models import Usuarios
 
-from django.http import HttpResponseNotAllowed, JsonResponse
+from django.http import HttpResponse,HttpResponseNotAllowed, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
@@ -29,3 +29,50 @@ def mostrar_lista_puntuacion():
 
 def ordenar_json(punt):
 	return punt["puntuacion"]
+	
+	
+	
+def login(request, nombre):
+	#Es POST?, si no lo es devuelve un error
+	if request.method != 'POST':
+		return HttpResponseNotAllowed(['POST'])
+	# Coje un usuario y mira si existe, y si no existe salta el  404
+	try: 
+		usuario = User.objects.get(nombre__exact=nombre)
+	except Usuario.DoesNotExist:
+		return JsonResponse({"errorDescription": "Usuario no encontrado, prueba a registrarte o inténtalo más tarde"}, status=404)
+
+	cuerpo_solicitud = json.loads(request.body)
+	contrasenaPeticion = cuerpo_solicitud.get('password')
+	contrasenaBBDD = usuario.contrasena
+
+	if contrasenaPeticion == contrasenaBBDD:
+		# Genero un token de sesion
+		random = secrets.token_urlsafe(64)
+		respuesta = {"session_cookie": random}
+		usuario.token_sesion = random
+		usuario.save()
+		return JsonResponse(mi_respuesta, status=200)
+		
+	return JsonResponse({"errorDescription": "Contraseña incorrecta"}, status=401)
+
+def registro(request):
+	# Si recibimos una peticion que no es POST, devolvemos un 405
+	if request.method != 'POST':
+		return HttpResponseNotAllowed(['POST'])
+	try:
+		usuario=Usuario.objects.get(nombre__exact=nombre)
+		return JsonResponse(("Nombre de usuario ya existe"),status=422)
+	except Usuario.DoesNotExist:
+		#obtener la contraseña que quieres asignar
+		cuerpo_solicitud = json.loads(request.body)
+		contrasenaPeticion = cuerpo_solicitud.get('password')
+		usuario=Usuario.get.object(nombre='')
+		#lanzar un token de sesion
+		usuario.token_sesion = random
+		usuario.save()
+		password.save()
+		return JsonResponse(("Usuario creado correctamente"),status=201)
+	return JsonResponse(("No se ha podido leer la contraseña o el nombre de usuario"),status=400)
+
+
--- Django_v1/TormentorXUbermosh_mysite/fachada_rest/urls.py
+++ Django_v1/TormentorXUbermosh_mysite/fachada_rest/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import rest_facade
 
 urlpatterns = [
-    path('puntuacion', rest_facade.obtener_puntuacion, name='get_puntuacion')
+    path('puntuacion', rest_facade.obtener_puntuacion, name='get_puntuacion'),
+    path('usuario',rest_facade.login,name='login'),
+    path('usuario/{username}',rest_facade.registro,name='registro'),
 ]
