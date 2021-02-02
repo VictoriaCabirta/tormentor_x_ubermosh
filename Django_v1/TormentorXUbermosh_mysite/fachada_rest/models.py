70dad6b64f1be4f32e4adbaf3ba9a1712933c562
Victoria <mcabirtaa@fpcoruna.afundacion.org>
2021-02-02 10:05:02 +0100

    Actualizado el modelo Usuario

--- Django_v1/TormentorXUbermosh_mysite/fachada_rest/models.py
+++ Django_v1/TormentorXUbermosh_mysite/fachada_rest/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 class Usuarios(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
-    contrasena = models.CharField(max_length=100)
     puntuacion = models.IntegerField(default=0)
+    contrasena_hmac = models.BinaryField()
+    salt = models.BinaryField()
+    token_sesion = models.CharField(max_length=200)
 
+def __str__(self):
+		return self.nombre
