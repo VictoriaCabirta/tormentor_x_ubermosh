ed796c9f93d617f6bbeda356256c95f297bafae2
Victoria <mcabirtaa@fpcoruna.afundacion.org>
2021-02-02 13:19:00 +0100

    Creación de varios modelos de API

--- Django_v1/TormentorXUbermosh_mysite/fachada_rest/models.py
+++ Django_v1/TormentorXUbermosh_mysite/fachada_rest/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 class Usuarios(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
-    contrasena = models.CharField(max_length=100)
     puntuacion = models.IntegerField(default=0)
+    contrasena_hmac = models.BinaryField()
+    salt = models.BinaryField()
+    tokenSesion = models.CharField(max_length=200)
+    verificado = models.BooleanField(default=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        representacion_en_string = self.nombre
+        if self.verificado == True:
+            representacion_en_string = representacion_en_string + " [Verificado]"
+        else:
+            representacion_en_string = representacion_en_string + " [No Verificado]"
+        return representacion_en_string
