a8a9d5f44f02cabee3f5d119afb3456dfbc1ba44
antonio.rodriguez <45874039X>
2021-02-07 15:37:17 +0100

    Dto de puntuaciones que no se si necesita tener más atributos

---
+++ TormentorXUbermosh/NativeAndroidApp/app/src/main/java/com/unity/mynativeapp/RespuestaPuntuacionesDto.java
@@ -0,0 +1,13 @@
+package com.unity.mynativeapp;
+
+public class RespuestaPuntuacionesDto {
+    private int puntuacion;
+
+    public RespuestaPuntuacionesDto(int puntuacion){
+        this.puntuacion=puntuacion;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+}
