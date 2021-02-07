a50f2d451edb6c5490ff71f931563fa2cec2fcd8
Victoria Cabirta <mcabirtaa@fpcoruna.afundacion.org>
2021-02-07 18:20:41 +0100

    Modificado nombre url de la web

--- Django_v1/TormentorXUbermosh_mysite/fachada_rest/urls.py
+++ Django_v1/TormentorXUbermosh_mysite/fachada_rest/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 from . import rest_facade
 
-app_name = 'fachada_rest'
 urlpatterns = [
     path('puntuacion', rest_facade.obtener_puntuacion, name='get_puntuacion'),
-    path('tormentorxubermosh/', views.IndexView.as_view(), name='tormentorxubermosh'),
+    path('tormentorxubermosh/', views.IndexView.as_view(), name='main_view'),
 ]
