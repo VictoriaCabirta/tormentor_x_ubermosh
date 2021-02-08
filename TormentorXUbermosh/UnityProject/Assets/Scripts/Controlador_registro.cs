6a5f7ebc7c5e6691b3bde00838b01e3a33dc4097
PyramidHead01 <hugo4444333@gmail.com>
2021-02-08 11:57:27 +0100

    21.02.08-11:57 Cambiando interfaces de acuerdo a la issue 21

--- TormentorXUbermosh/UnityProject/Assets/Scripts/Controlador_registro.cs
+++ TormentorXUbermosh/UnityProject/Assets/Scripts/Controlador_registro.cs
@@ -1,31 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Controlador_registro : MonoBehaviour{
-	public GameObject menuMuerte , registro ;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	
+	public GameObject menuMuerte , registro, subPunt;
+	public InputField textNombre, textContr;
+	public Toggle NuevoUsuario;
 	
 	public void Registrarse(){
 		menuMuerte.SetActive(false);
 		registro.SetActive(true);
-		
 	}
 	
 	public void Volver(){
 		registro.SetActive(false);
 		menuMuerte.SetActive(true);
-		
-		
 	}
+
+	public void EnviarUsuario()
+	{
+		registro.SetActive(false);
+		subPunt.SetActive(true);
+
+		Debug.Log(textNombre.text);
+		Debug.Log(textContr.text);
+
+	}
+
+	public void SubirPuntuacion()
+    {
+
+		if (NuevoUsuario.isOn)
+		{
+			Debug.Log("Se ha creado un nuevo usuario");
+			//Metodo posto para nuevo usuario
+		}
+		else
+		{
+			Debug.Log("Se ha actualizado la puntuacion de un usuario");
+			//Metodo post usuario ya existente
+		}
+
+		NoSubirPuntuacion();
+    }
+
+	public void NoSubirPuntuacion()
+    {
+		subPunt.SetActive(false);
+		menuMuerte.SetActive(true);
+	}
+
 }
