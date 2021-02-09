64325db26661cbb75fdadfecc4745c1eab9e9c63
antonio.rodriguez <arodriguezr@fpcoruna.afundacion.org>
2021-02-09 12:54:00 +0100

    #9 solucionado

--- Django_v1/TormentorXUbermosh_mysite/fachada_rest/models.py
+++ Django_v1/TormentorXUbermosh_mysite/fachada_rest/models.py
@@ -4,6 +4,6 @@ import os
 class Usuarios(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
     puntuacion = models.IntegerField(default=0)
-    contrasena = models.CharField(max_length=100,null=True)
+    contrasena = models.CharField(max_length=100,null=False)
 
 
