94fb0fc60ed3af28115fe5d484fb435b1fd93463
Victoria <mcabirtaa@fpcoruna.afundacion.org>
2021-02-10 09:19:43 +0100

    Cambiado el nombre del método principal

--- Django_v1/TormentorXUbermosh_mysite/fachada_rest/urls.py
+++ Django_v1/TormentorXUbermosh_mysite/fachada_rest/urls.py
@@ -4,6 +4,6 @@ from . import rest_facade
 
 app_name = 'fachada_rest'
 urlpatterns = [
-    path('tormentorxubermosh', views.index, name="main_view_puntuacion"),
+    path('tormentorxubermosh', views.pantallaPuntuaciones, name="main_view_puntuacion"),
     path('puntuacion', rest_facade.obtener_puntuacion, name='get_puntuacion'),
 ]
--- Django_v1/TormentorXUbermosh_mysite/fachada_rest/views.py
+++ Django_v1/TormentorXUbermosh_mysite/fachada_rest/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Usuarios
 
-def index(request):
+def pantallaPuntuaciones(request):
     usuarios_list = Usuarios.objects.order_by('puntuacion')[:20]
     output = {'usuarios_list': usuarios_list, 'puntuacion': usuarios_list}
     return render(request, 'fachada_rest/tormentorxubermosh.html', output)
