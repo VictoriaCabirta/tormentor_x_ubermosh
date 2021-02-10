8ce243efc14c19f51c444905ad2343d5fc638a7a
Victoria <mcabirtaa@fpcoruna.afundacion.org>
2021-02-10 09:06:28 +0100

    Añadiendo puntuaciones a la view

--- Django_v1/TormentorXUbermosh_mysite/fachada_rest/views.py
+++ Django_v1/TormentorXUbermosh_mysite/fachada_rest/views.py
@@ -3,5 +3,6 @@ from .models import Usuarios
 
 def index(request):
     usuarios_list = Usuarios.objects.order_by('puntuacion')[:20]
-    output = {'usuarios_list': usuarios_list}
+    output = {'usuarios_list': usuarios_list, 'puntuacion': usr.puntuacion }
     return render(request, 'fachada_rest/tormentorxubermosh.html', output)
+
